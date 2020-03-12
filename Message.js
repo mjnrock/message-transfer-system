9d0a9f24b30324582b4e2fe8cba9ac8312e69c3f
Matt Nawrocki <mjnrock@umich.edu>
2020-03-12 18:35:53 -0400

    init

---
+++ Message.js
@@ -0,0 +1,30 @@
+export default class Message {
+    constructor(type, payload, destination, source = null) {
+        this.type = type;
+        this.payload = payload;
+        this.destination = destination;
+        this.source = source;
+        this.timestamp = Date.now();
+
+        return Object.freeze(this);
+    }
+
+    toJson() {
+        return JSON.stringify(this);
+    }
+
+    static fromJson(json) {
+        let obj = json;
+
+        while(typeof obj === "string" || obj instanceof String) {
+            obj = JSON.parse(obj);
+        }
+        
+        return new Message(
+            obj.type,
+            obj.payload,
+            obj.destination,
+            obj.source
+        );
+    }
+};
---
+++ Packet.js
@@ -0,0 +1,36 @@
+import Message from "./Message";
+
+export default class Packet extends Message {
+    static Types = {
+        DEFAULT: 1,
+        WEBSOCKET: 2,
+    };
+
+    constructor(type, payload, destination, source, protocol = Packet.Types.DEFAULT) {
+        super(type, payload, destination, source);
+        
+        this.protocol = protocol;
+
+        return Object.freeze(this);
+    }
+
+    toJson() {
+        return JSON.stringify(this);
+    }
+
+    static fromJson(json) {
+        let obj = json;
+
+        while(typeof obj === "string" || obj instanceof String) {
+            obj = JSON.parse(obj);
+        }
+        
+        return new Packet(
+            obj.type,
+            obj.payload,
+            obj.destination,
+            obj.source,
+            obj.protocol
+        );
+    }
+};
