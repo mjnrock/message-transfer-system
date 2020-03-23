5e3ec788d03c75316ad205e27adc0514ec639b62
Matt Nawrocki <mjnrock@umich.edu>
2020-03-23 11:47:43 -0400

    init

--- src/browser/package.js
+++ src/browser/package.js
@@ -1,9 +1,11 @@
 import GeoLocationNode from "./GeoLocationNode";
+import BroadcastChannelNode from "./BroadcastChannelNode";
 
 import Input from "./input/package";
 
 export default {
     GeoLocationNode,
-    
+    BroadcastChannelNode,
+
     Input
 };
