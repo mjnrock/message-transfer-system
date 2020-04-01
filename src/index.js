285ebf0b8548a8b66178352af4bf7b8cfe948fdc
Matt Nawrocki <mjnrock@umich.edu>
2020-04-01 15:09:52 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-import Helper from "./helper";
+import ALL from "./package";
 
-import Message from "./Message";
-import Node from "./Node";
-import Repeater from "./Repeater";
-import Registry from "./Registry";
-import Router from "./Router";
-
-import Main from "./Main";
-
-import Browser from "./browser/package";
-import Network from "./network/package";
-
-export default {
-    Helper,
-    
-    Message,
-    Node,
-    Repeater,
-    Registry,
-    Router,
-
-    Main,
-    
-    Browser,
-    Network
-};
+export default ALL;
