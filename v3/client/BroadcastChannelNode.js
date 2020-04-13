ec60b94296863ecbb6796eea54d1ea8dedd4f606
Matt Nawrocki <mjnrock@umich.edu>
2020-04-13 19:56:28 -0400

    init

---
+++ v3/client/BroadcastChannelNode.js
@@ -0,0 +1,96 @@
+import { GenerateUUID } from "./../util/helper";
+import Node from "./../Node";
+import Message from "./../Message";
+
+export default class BroadcastChannelNode extends Node {
+    static SignalTypes = {
+        MESSAGE: "BroadcastChannelNode.Message",
+        MESSAGE_ERROR: "BroadcastChannelNode.MessageError",
+        ERROR: "BroadcastChannelNode.Error",
+    };
+    
+    static AllSignalTypes(...filter) {
+        return Object.values(BroadcastChannelNode.SignalTypes).filter(st => {
+            if(filter.includes(st)) {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    constructor({ name = null, receive = null, mnode = null, packager = null } = {}) {
+        super({
+            name: name || GenerateUUID(),
+            receive: receive,
+            mnode: mnode,
+            packager: packager
+        });
+
+        this.supply = {
+            Name: null,
+            Channel: null
+        };
+    }
+
+    getName() {
+        return this.supply.Name || false;
+    }
+
+    connect(name) {
+        let bc = new BroadcastChannel(name);
+
+        bc.onmessage = this._onBcMessage.bind(this);
+        bc.onmessageerror = this._onBcMessageError.bind(this);
+
+        this.supply.Name = name;
+        this.supply.Channel = bc;
+
+        return this;
+    }
+    disconnect() {
+        if(this.supply.Channel instanceof BroadcastChannel) {
+            this.supply.Channel.close();
+
+            this.supply.Name = null;
+            this.supply.Channel = null;
+        }
+
+        return this;
+    }
+
+    receive(msg) {
+        if(Message.conforms(msg) && msg.type === BroadcastChannelNode.SignalTypes.MESSAGE) {
+            this.bcmessage(msg.payload);
+        }
+    }
+    bcsend(type, payload) {
+        let msg = new Message(type, payload, this.signet);
+        
+        this.bcmessage(msg);
+    }
+    bcmessage(msg) {
+        try {
+            if(Message.conforms(msg)) {
+                this.supply.Channel.postMessage(msg);
+            }
+        } catch(e) {
+            this.emit(BroadcastChannelNode.SignalTypes.ERROR, e);
+        }
+    }
+
+    _onBcMessage(e) {        
+        try {
+            let msg = e.data;
+
+            if(Message.conforms(msg)) {
+                this.emit(msg);
+            }
+        } catch(e) {
+            this._onBcMessageError(e);
+        }
+    }
+    _onBcMessageError(e) {
+        this.emit(BroadcastChannelNode.SignalTypes.MESSAGE_ERROR, e);
+    }
+};
---
+++ v3/client/package.js
@@ -0,0 +1,17 @@
+import KeyboardNode from "./KeyboardNode";
+import MouseNode from "./MouseNode";
+import TouchNode from "./TouchNode";
+import GeoLocationNode from "./GeoLocationNode";
+import CanvasNode from "./_WIP_CanvasNode";
+import QRCodeNode from "./QRCodeNode";
+import BroadcastChannelNode from "./BroadcastChannelNode";
+
+export default {
+    KeyboardNode,
+    MouseNode,
+    TouchNode,
+    GeoLocationNode,
+    CanvasNode,
+    QRCodeNode,
+    BroadcastChannelNode,
+};
